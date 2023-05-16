264c6fbf5a267a07a28030da16bf45bfc1693566
Thomas <thomaslyy13@gmail.com>
2023-05-16 15:17:36 -0700

    finished phase 2

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase_1_arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase_1_arrays.js
@@ -1,1 +1,45 @@
 // Your code here
+Array.prototype.uniq = function() {
+  const new_arr = [this[0]];
+
+  for (let i = 1; i < this.length; i++) {
+      if (!new_arr.includes(this[i])) {
+          new_arr.push(this[i]);
+      };
+  };
+  return new_arr;
+};
+
+Array.prototype.twoSum = function() {
+  const sums = [];
+
+  for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length; j++) {
+          if (j > i && this[i] + this[j] === 0) {
+              sums.push([i, j]);
+          };
+          
+      };
+  };
+
+  return sums;
+};
+
+
+// console.log([-1, 0, 2, -2, 1].twoSum())
+
+Array.prototype.transpose = function () {
+
+  const transposed = Array.from(Array(this.length), () => new Array(this.length));
+
+  for (let i =0; i < this.length; i++) {
+      for (let j =0; j <this.length; j++) {
+              transposed[i][j] = this[j][i];
+
+      };
+
+  };
+  return transposed;
+};
+
+console.log([[0, 1, 2], [3, 4, 5], [6, 7, 8]].transpose())
--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase_2_enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase_2_enumerable.js
@@ -1,1 +1,50 @@
-// Your code here
+// // Your code here
+// Array.prototype.uniq = function() {
+//   const new_arr = [this[0]];
+
+//   for (let i = 1; i < this.length; i++) {
+//       if (!new_arr.includes(this[i])) {
+//           new_arr.push(this[i]);
+//       };
+//   };
+//   return new_arr;
+// };
+
+Array.prototype.myEach = function(callback) {
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i]);
+  };
+};
+
+
+
+
+// console.log([3, 4, 5, 3, 4].myEach(logElement))
+
+Array.prototype.myMap = function(callback) {
+  const results = [];
+
+  this.myEach(function(ele){
+    results.push(callback(ele))
+  })
+  return results;
+};
+
+function logElement(el) {
+  return (el + 2);
+};
+
+// console.log([3, 4, 5, 3, 4].myMap(logElement));
+
+Array.prototype.myReduce = function(callback, ...starting) {
+  let sum = 0;
+  this.myEach(function(ele) {
+    acc = sum;
+    sum = callback(acc, ele);
+  });
+  return sum + starting;
+};
+
+console.log([1, 2, 3].myReduce(function(acc, el) {
+  return acc + el;
+}));
