3abc33d6f56a3fb722bba315acf16128332d39cb
Your Name <129026648+Vneet11@users.noreply.github.com>
2023-05-16 16:09:49 -0700

    phase 3 done

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase_3_iteration.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase_3_iteration.js
@@ -1,1 +1,33 @@
 // Your code here
+
+Array.prototype.bubbleSort = function () {
+ 
+    let sorted = false;
+    while (sorted === false) {
+        sorted = true;
+        for (i = 0; i < this.length; i++) {
+            if (this[i] > this[i+1]) {
+                let temp = this[i]
+                this[i] = this[i+1]
+                this[i+1] = temp
+                sorted = false;
+            };
+        };
+    
+    };
+    return this;
+};
+
+// console.log([0, 3, 8, 6, 9, 2].bubbleSort());
+
+String.prototype.substrings = function() {
+    const substrings = [];
+    for (let i = 0; i < this.length; i++) {
+        for (let j = i+1; j < this.length + 1; j++ ) {
+            substrings.push(this.slice(i, j))
+        };
+    };
+    return substrings;
+};
+
+console.log(('paulo').substrings());
